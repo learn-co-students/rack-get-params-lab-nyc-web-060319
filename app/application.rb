35e4c6447977aca30b3b11ffe19a07e1691c7eea
CindyChenx <chen_cx0128@hotmail.com>
2019-06-23 22:20:50 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -15,14 +15,14 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
-      add_item = req.param["items"]
-        if @@items.include?(add_item)
-          @@cart << add_item
-          resp.write "added #{add_item}"
+      search = req.params["item"]
+        if @@items.include?(search)
+          @@cart << search
+          resp.write "added #{search}"
         else
-          resp.write "We do not have that item"
+          resp.write "We don't have that item"
         end
-    elseif req.parh.match(/cart/)
+    elsif req.path.match(/cart/)
         if @@cart.length > 0
           @@cart.each {|item| resp.write "#{item}\n"}
         else
